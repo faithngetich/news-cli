f0a9948d635eb9d8f9fecdc03a118c9639ab25db
faithngetich <faithngetich188@gmail.com>
2018-07-13 15:16:57 +0300

    Output different categories

--- news.rb
+++ news.rb
@@ -1,22 +1,56 @@
-
-require "open-uri"
-require 'json'
-require 'news-api'
-
-n = News.new("398565600ab6428980cda6fdf51f13f4")
+require 'rubygems'
+require 'httparty'
 
 class NewsApi
+  include HTTParty
+  base_uri "newsapi.org/"
 
   def getsheadlines
-    response = open("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=398565600ab6428980cda6fdf51f13f4").read
-    JSON.parse(response)
+    self.class.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=398565600ab6428980cda6fdf51f13f4')
   end
 
-  def gets_entertainment
-    response = open("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=398565600ab6428980cda6fdf51f13f4").read
-    JSON.parse(response)
+  def get_entertainment_news
+    self.class.get('https://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=398565600ab6428980cda6fdf51f13f4')
   end
+
+  def get_business_news
+    self.class.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=398565600ab6428980cda6fdf51f13f4')
+  end
+
+  def get_general_news
+    self.class.get('https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=398565600ab6428980cda6fdf51f13f4')
+  end
+
+  def get_science_news
+    self.class.get('https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=398565600ab6428980cda6fdf51f13f4')
+  end
+
+  
 end
 
 newsapi = NewsApi.new
 puts newsapi.getsheadlines
+puts '======================================'
+puts '======================================'
+puts '======================================'
+puts '======================================'
+puts '======================================'
+puts newsapi.get_general_news
+puts '======================================'
+puts '======================================'
+puts '======================================'
+puts '======================================'
+puts '======================================'
+puts '======================================'
+
+puts newsapi.get_business_news
+puts '======================================'
+puts '======================================'
+puts '======================================'
+puts '======================================'
+puts '======================================'
+puts '======================================'
+puts newsapi.get_science_news
+puts '======================================'
+puts '======================================'
+puts '======================================'
